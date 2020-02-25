623598a2ec2b0df806305c3b4c5b936a05e25fa3
huytd <huytd6627@massgenie.com>
2020-02-25 10:15:47 +0700

    add observer pattern example: weather station

---
+++ WeatherAppTest/src/com/weather/WeatherStation.java
@@ -0,0 +1,12 @@
+package com.weather;
+
+public class WeatherStation {
+    public static void main(String[] args){
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 60, 30.4f);
+        weatherData.setMeasurements(90, 65, 40.4f);
+        weatherData.setMeasurements(85, 50, 20.4f);
+    }
+}
